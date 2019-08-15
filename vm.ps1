2132fdadc6e1d52bc8904f968bbf6b7ba3dfa510
nareshtyse <48803828+nareshtyse@users.noreply.github.com>
2019-08-15 13:37:31 -0400

    Create vm.ps1

---
+++ vm.ps1
@@ -0,0 +1,11 @@
+
+Connect-AzAccount
+New-AzVm `
+    -ResourceGroupName "myResourceGroup" `
+    -Name "myVM" `
+    -Location "East US" `
+    -VirtualNetworkName "myVnet" `
+    -SubnetName "mySubnet" `
+    -SecurityGroupName "myNetworkSecurityGroup" `
+    -PublicIpAddressName "myPublicIpAddress" `
+    -OpenPorts 80,3389
